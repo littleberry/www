82aca494b88f6a25145341ed26edef24a729735a
Patricia Kruep <pkruep@gmail.com>
2013-09-24 15:27:06 -0400

    commented out add contact functionality for now.

--- ui/client-controls.js
+++ ui/client-controls.js
@@ -20,36 +20,54 @@ $(document).ready( function() {
 		}
 	});
 	
-	var $cancelContact = $( '<li class="client-details-item cancel-additional"></li>' );
-	var $cancelContactLabel = $( '<label for="cancel-contact-link" class="client-details-label">Need to remove contact?</label>' ).appendTo( $cancelContact );
-	var $cancelContactLink = $( '<a id="cancel-contact-link" class="cancel-action-link" href="#" tabindex="19">Cancel</a>' ).click( function( evt ) {
-		console.log('cancel');
-		//$( '.contact-details-list' ).find( '[type="checkbox"]' ).prop( 'checked', false );
-		//$ ( this ).prop( 'checked', true )
-		//if ( $( this ).parent() )
-		/*
-if ( $( this ).parent().next().has( '#add-additional-link' ) ) {
-			var addNewContactLink = $( this ).parent().next( ).detach();
-			$( this ).parents( '.contact-details-entry' ).remove();
-			$( '#contact-detail' ).last().find( '.contact-details-list' ).append( addNewContactLink );
-		}
+	/*
+var $cancelContact = $( '<li class="client-details-item cancel-additional"></li>' );
+	var $cancelContactLabel = $( '<label for="cancel-contact-link" class="client-details-label">Need to remove contact?</label>' )
+		.appendTo( $cancelContact );
+	var $cancelContactLink = $( '<a id="cancel-contact-link" class="cancel-action-link" href="#" tabindex="19">Cancel</a>' )
+		.click( function( evt ) {
+			console.log('cancel');
+			//$( '.contact-details-list' ).find( '[type="checkbox"]' ).prop( 'checked', false );
+			//$ ( this ).prop( 'checked', true )
+			//if ( $( this ).parent() )
+			/*
+	if ( $( this ).parent().next().has( '#add-additional-link' ) ) {
+				var addNewContactLink = $( this ).parent().next( ).detach();
+				$( this ).parents( '.contact-details-entry' ).remove();
+				$( '#contact-detail' ).last().find( '.contact-details-list' ).append( addNewContactLink );
+			}
+	*/
+			
+			evt.preventDefault();
+		})
+		.appendTo( $cancelContact );
 */
-		
-		evt.preventDefault();
-	}).appendTo( $cancelContact );
 	
+/*
 
 	$( '#add-additional-link' ).click( function( evt ) {
-		var $newContactDetailsForm = $( '.contact-details-entry' ).last().clone( true );
-		$( this ).parent().replaceWith( $cancelContact );
+		var $newContactDetailsForm = $( '.contact-details-entry' )
+			.last()
+			.clone( true );
+
+		$( this )
+			.parent()
+			.replaceWith( $cancelContact );
 		
-		$newContactDetailsForm.find( '[type="checkbox"]' ).prop( 'checked', false );
-		$newContactDetailsForm.find( '[type="text"]' ).val( '' );
-		$( '.contact-details-entry' ).last().after( $newContactDetailsForm );
-		console.log($( '.contact-details-entry .add-additional' ).length + ", " + $( '.contact-details-entry .cancel-additional' ).length)
+		$newContactDetailsForm
+			.find( '[type="checkbox"]' )
+			.prop( 'checked', false )
+			.end()
+			.find( '[type="text"]' )
+			.val( '' );
+		
+		$( '.contact-details-entry' )
+			.last()
+			.after( $newContactDetailsForm );
 		
 		evt.preventDefault();
 	});
+*/
 	
 	
 	
