b00f3959a93db642389cc905b3a0160c55b4995e
catsbap <catsbap@gmail.com>
2013-10-07 22:26:30 -0400

    changed the login page to get the $_SERVER referrer.

--- ui/index.php
+++ ui/index.php
@@ -2,7 +2,7 @@
 
 	require_once("../common/common.inc.php");
 	require_once("../classes/Person.class.php");
-	checkLogin();
+	checkLogin(basename($_SERVER["PHP_SELF"]));
 
 	
 ?>
@@ -39,6 +39,8 @@
 </header>
 <section id="page-content" class="page-content">
 This page will eventually be the dashboard
+<a href="logout.php"><br><br>LOG OUT</a>
+
 </section>
 <footer id="site-footer" class="site-footer">
 
