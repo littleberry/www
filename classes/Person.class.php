f0bc7e205a0fa88128b4c9e0168e9411fdb88e59
catsbap <catsbap@gmail.com>
2013-10-12 10:17:40 -0400

    added some new functions for people path

--- classes/Person.class.php
+++ classes/Person.class.php
@@ -6,7 +6,7 @@ require_once("DataObject.class.php");
 
 class Person extends DataObject {
 	protected $data = array(
-		//these fields are in the client table.
+		//these fields are in the person table.
 		"person_id"=>"",
 		"person_username"=>"",
 		"person_password"=>"",
@@ -39,32 +39,58 @@ class Person extends DataObject {
 			die("query failed: " . $e->getMessage() );
 		}
 	}
+	
+	//gets people out of the client table. We'll return this as an array and put it in a list once the
+	//data is out.
+	public static function getPeople() {
+		$conn=parent::connect();
+		$sql = "SELECT * FROM " . TBL_PERSON ;
+		try {
+			$st = $conn->prepare($sql);
+			$st->execute();
+			$person=array();
+			foreach ($st->fetchAll() as $row) {
+				$person[] = new Person($row);
+			}
+			parent::disconnect($conn);
+			return array($person);
+		}catch(PDOException $e) {
+			parent::disconnect($conn);
+			die("query failed here: " . $e->getMessage() . "query is " . $sql);
+		}
+	}
 
+
+//the email address is the user's login. The user is sent an email address and are asked to set up their password before they login. The password is not set here,
+//it is set once the user gets the email.
 	public function insertPerson() {
 	//first insert the parent row
-		//removed foreign key on person_permissions. Not 100% sure why I designed it that way!!
+		//removed foreign key on person_permissions. Consider what will happen with permissions!
 		$conn=parent::connect();
 		$sql = "INSERT INTO " . TBL_PERSON . " (
-			person_username,
-			person_password,
 			person_first_name,
 			person_last_name,
-			person_email
+			person_email,
+			person_department,
+			person_hourly_rate,
+			person_perm_id
 			) VALUES (
-			:person_username,
-			password(:person_password),
 			:person_first_name,
 			:person_last_name,
-			:person_email
+			:person_email,
+			:person_department,
+			:person_hourly_rate,
+			:person_perm_id
 			)";
 			
 		try {
 			$st = $conn->prepare($sql);
-			$st->bindValue(":person_username", $this->data["person_username"], PDO::PARAM_STR);
-			$st->bindValue(":person_password", $this->data["person_password"], PDO::PARAM_STR);
 			$st->bindValue(":person_first_name", $this->data["person_first_name"], PDO::PARAM_STR);
 			$st->bindValue(":person_last_name", $this->data["person_last_name"], PDO::PARAM_STR);
 			$st->bindValue(":person_email", $this->data["person_email"], PDO::PARAM_STR);
+			$st->bindValue(":person_department", $this->data["person_department"], PDO::PARAM_STR);
+			$st->bindValue(":person_hourly_rate", $this->data["person_hourly_rate"], PDO::PARAM_INT);
+			$st->bindValue(":person_perm_id", $this->data["person_perm_id"], PDO::PARAM_STR);
 			$st->execute();
 			parent::disconnect($conn);
 		} catch (PDOException $e) {
@@ -73,6 +99,7 @@ class Person extends DataObject {
 		}
 	}
 
+//this seems like a duplicate of getByUserName. Can we delete this?
 public static function getPerson($person_username) {
 		$conn=parent::connect();
 		$sql = "SELECT * FROM " . TBL_PERSON . " WHERE person_username = :person_username";
@@ -93,8 +120,7 @@ public static function getPerson($person_username) {
 		}
 	}
 	
-	//function added as part of the member registration application
-	public static function getByUserName($person_username) {
+public static function getByUserName($person_username) {
 		$conn=parent::connect();
 		$sql = "SELECT * FROM " . TBL_PERSON . " WHERE person_username = :person_username";
 	
@@ -112,7 +138,6 @@ public static function getPerson($person_username) {
 		}
 	}
 
-	//function added as part of the member registration application
 	public static function getByEmailAddress($person_email) {
 		$conn=parent::connect();
 		$sql = "SELECT * FROM " . TBL_PERSON . " WHERE person_email = :person_email";
@@ -130,6 +155,26 @@ public static function getPerson($person_username) {
 			die("Query failed: " . $e->getMessage());
 		}
 	}
+	//function returns the individual person types
+	public static function getPersonTypes() {
+		$conn=parent::connect();
+		$sql = "SELECT distinct(person_type) FROM " . TBL_PERSON;
+	
+	
+		try {
+			$st = $conn->prepare($sql);
+			$st->execute();
+			$person=array();
+			foreach ($st->fetchAll() as $row) {
+				$person[] = new Person($row);
+			}
+			parent::disconnect($conn);
+			return array($person);
+		}catch(PDOException $e) {
+			parent::disconnect($conn);
+			die("query failed here: " . $e->getMessage() . "query is " . $sql);
+		}	}
+	
 }
 
 ?>
