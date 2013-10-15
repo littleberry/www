6b1e0295d665830eca16e66821d76191e65292e9
catsbap <catsbap@gmail.com>
2013-10-15 14:44:04 -0400

    added new user cake auth

--- common/common.inc.php
+++ common/common.inc.php
@@ -1,41 +1,16 @@
 <?php
 
-/*
-function displayPageHeader($pageTitle) {
-?>
-<!doctype html public "-//w3c//dtd html 1.0 strict//en" "http://www.w3.org/TR/xhtml1-strict.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
-<head>
-<title><?php echo $pageTitle?></title>
-<link rel="stylesheet" type="text/css" href="/common/common.css" />
-<style type="text/css">
-th {text-align:left; background-color:#bbb;}
-th,td {padding:0.4em;}
-tr.alt td {background: #ddd;}
-.error {background:#d33; color:white; padding:0.2em;}
-</style>
-</head>
-<body>
-<h1><?php echo $pageTitle?></h1>
-<?php
-}
-
-
-//display page footer information
-function displayPageFooter() {
-?>
-</body>
-</html>
-<?php
-}
-
-*/
-
-function displayPageHeader($pageTitle, $membersArea = false) {
-}
+//function displayPageHeader($pageTitle, $membersArea = false) {
+//}
 
 ini_set ('display_errors', 0);
-require_once("../classes/Person.class.php");
+
+
+//THIS FILE IS ALWAYS INCLUDED, SO GET THE CONFIG FOR THE AUTH.
+require_once($_SERVER["DOCUMENT_ROOT"] . "/time_tracker/usercake/models/config.php");
+if (!securePage($_SERVER['PHP_SELF'])){die();}
+
+require_once($_SERVER["DOCUMENT_ROOT"] . "/time_tracker/classes/Person.class.php");
 
 
 //is the value in the missing field array? If so, highlight the field using the "error" style..
@@ -50,7 +25,6 @@ function setChecked(DataObject $obj, $fieldName, $fieldValue) {
 	if ($obj->getValue($fieldName) == $fieldValue) {
 		echo ' checked=checked';
 	}
-	//echo "CALLED ERT";
 }
 
 function setSelected(DataObject $obj, $fieldName, $fieldValue) {
@@ -80,6 +54,5 @@ function checkLogin($page) {
 		//$logEntry->record();
 	//}
 }
-
 
 ?>
