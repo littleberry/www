ec43875ef7ec149211aa1b5ac821b309f85507ac
Patricia Kruep <pkruep@gmail.com>
2013-10-15 23:17:59 -0400

    Projects list html template, html updates to php, css, jquery and tablesort jquery plugin

--- ui/projects.php
+++ ui/projects.php
@@ -7,6 +7,7 @@
 		//redirect if user is not logged in.
 		$_SESSION["redirect"] = $_SERVER["PHP_SELF"];
 		header( 'Location: http://localhost:8888/time_tracker/usercake/login.php' ) ;
+		//header( 'Location: http://strawberry.dev/MBTimeTtracker/usercake/login.php' ) ;
 	}
 	
 	?>
@@ -21,30 +22,39 @@
 			</ul>
 		</nav>
 	</header>
-		<?php $clientList = Project::getClientsProjectsByStatus(0);
-			foreach($clientList as $clients) {
-		?>
 	<section class="content">
-		<ul id="project-list" class="entity-list project">
-			<li class="entity-list-item project l-col-33">
-				<ul class="entity-info-list project">
-					<li class="client-info-contact">Client: <a class="client-info-contact-link" href="<?php echo "client-detail.php?client_id=" . $clients["client_id"]?>" title="View client details"><?php echo $clients["client_name"] ?></li>
-					<?php 
+		<table id="project-list" class="entity-table projects tablesorter">
+			<thead>
+				<tr>
+					<!-- <td><input id="select-all" name="select-all" type="checkbox" value="all" title="Select all projects" /></td> -->
+					<td>Project</td>
+					<td>Client</td>
+					<td>Hours/Budget</td>
+				</tr>
+			</thead>
+			<tbody>
+			<?php $clientList = Project::getClientsProjectsByStatus(0);
+				foreach($clientList as $clients) {
 					//get the array of project objects for this client.
 					$projects = Project::getProjectByClientId($clients["client_id"]);
 					foreach ($projects as $project) { 
 					?>
-						<li class="project-info-name"><a class="project-info-name-link" href="<?php echo "project-detail.php?project_id=" . $project->getValueEncoded("project_id")?>" title="View project details"><?php echo $project->getValueEncoded("project_name")?></a></li>
+						<tr>
+							<!-- <td><input id="select-project" name="select-project" type="checkbox" value="all" title="Select project" /> -->
+							<td><a class="project-info-name-link" href="<?php echo "project-detail.php?project_id=" . $project->getValueEncoded("project_id")?>" title="View project details"><?php echo $project->getValueEncoded("project_name")?></a></td>
+							<td><a class="client-info-contact-link" href="<?php echo "client-detail.php?client_id=" . $clients["client_id"]?>" title="View client details"><?php echo $clients["client_name"] ?></a></td>
+							<td>x Hours/y budget</td>
+						</tr>
 					<?php } ?>
-				</ul>		
-			</li>
-		</ul>
+				<?php } ?>
+			</tbody>
+		</table>
 	</section>
-	<?php } ?>
+		
 </section>
 <footer id="site-footer" class="site-footer">
 
 </footer>
-
+<script src="project-controls.js" type="text/javascript"></script>
 </body>
 </html>
