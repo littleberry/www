9f94510911bfa49912d53c7efbfad795a282d76d
catsbap <catsbap@gmail.com>
2013-10-16 16:45:33 -0400

    removed auth added universal access

--- index.php
+++ index.php
@@ -1,16 +1,18 @@
 <?php
 	//put this in a general place, htdocs.
 	//change this if it is on muppetlabs or on localhost...
-	require_once($_SERVER["DOCUMENT_ROOT"] . "/time_tracker/common/common.inc.php");
-	require_once($_SERVER["SITE_BASE"] . $_SERVER["DOCUMENT"] . "/usercake/models/config.php");
-	require_once($_SERVER["SITE_BASE"] . $_SERVER["DOCUMENT"] . "/common/common.inc.php");
-	require_once($_SERVER["SITE_BASE"] . $_SERVER["DOCUMENT"] . "/classes/Person.class.php");
+	require_once($_SERVER["DOCUMENT_ROOT"] . "/common/common.inc.php");
+	require_once($_SERVER["DOCUMENT_ROOT"] . "/usercake/models/config.php");
+	require_once($_SERVER["DOCUMENT_ROOT"] . "/common/common.inc.php");
+	require_once($_SERVER["DOCUMENT_ROOT"] . "/classes/Person.class.php");
 
 if(!isUserLoggedIn()){
 	$_SESSION["redirect"] = $_SERVER["PHP_SELF"];
 	header( 'Location: usercake/login.php' ) ;
 
 }	
+
+
 //if(isset($_POST["action"]) and $_POST["action"] == "login") {
 //	processForm();
 //}else{
