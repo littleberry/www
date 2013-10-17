ff4f0060db9b4439e2ba994fa01dc28af07f135a
catsbap <catsbap@gmail.com>
2013-10-17 14:28:56 -0400

    relative urls

--- ui/people.php
+++ ui/people.php
@@ -1,6 +1,6 @@
 <?php
-	require_once($_SERVER["DOCUMENT_ROOT"] . "/common/common.inc.php");
-	require_once($_SERVER["DOCUMENT_ROOT"] . "/classes/Person.class.php");
+	require_once("../common/common.inc.php");
+	require_once("../classes/Person.class.php");
 		//remove auth
 		//if(!isUserLoggedIn()){
 		//redirect if user is not logged in.
