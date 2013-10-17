ac0518bcd9ddf921ce6ea84cf485d48f5fc09630
catsbap <catsbap@gmail.com>
2013-10-17 14:27:18 -0400

    relative urls

--- classes/DataObject.class.php
+++ classes/DataObject.class.php
@@ -3,7 +3,7 @@
 //objects that store and retrieve information from the DB
 
 //only require the config file once in the app.
-require_once($_SERVER["DOCUMENT_ROOT"] . "/config/config.php");
+require_once("../config/config.php");
 
 abstract class DataObject  {
 	//the array can be used by objects but not directly accessed. This is the array that holds the individual objects created with their values retrieved from the database.
