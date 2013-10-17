73ae2def3844f13dbc2450dcd17b5aa87d4cf61a
catsbap <catsbap@gmail.com>
2013-10-17 14:27:44 -0400

    relative urls

--- ui/index.php
+++ ui/index.php
@@ -1,15 +1,29 @@
 <?php
+	//put this in a general place, htdocs.
+	//change this if it is on muppetlabs or on localhost...
+	//require_once("common/common.inc.php");
+	//require_once($_SERVER["DOCUMENT_ROOT"] . "/usercake/models/config.php");
+	//require_once("common/common.inc.php");
+	require_once("../classes/Person.class.php");
 
-	require_once($_SERVER["DOCUMENT_ROOT"] . "/common/common.inc.php");
-	require_once($_SERVER["DOCUMENT_ROOT"] . "/classes/Person.class.php");
-	//checkLogin(basename($_SERVER["PHP_SELF"]));
+//if(!isUserLoggedIn()){
+//	$_SESSION["redirect"] = $_SERVER["PHP_SELF"];
+//	header( 'Location: usercake/login.php' ) ;
 
-	include('header.php'); //add header.php to page
+//}	
+
+
+//if(isset($_POST["action"]) and $_POST["action"] == "login") {
+//	processForm();
+//}else{
+//	displayForm(array(), array(), new Person(array()));
+//}
 ?>
-
+<!DOCTYPE html>
+	<?php	include('header.php'); //add header.php to page ?>
 <section id="page-content" class="page-content">
-This page will eventually be the dashboard
-<a href="logout.php"><br><br>LOG OUT</a>
+HELLO, I AM THE INDEX.PHP PAGE.
+THIS PAGE WILL EVENTUALLY BE THE DASHBOARD.
 
 </section>
 <footer id="site-footer" class="site-footer">
