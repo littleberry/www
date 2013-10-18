b9d08b59476594f6e12c296bd5829b9a6c57d80b
Patricia Kruep <pkruep@gmail.com>
2013-10-17 22:47:40 -0400

    added header.php include, removed extraneous html that wasn't needed.

--- ui/people.php
+++ ui/people.php
@@ -8,46 +8,15 @@
 		//header( 'Location: http://localhost:8888/time_tracker/usercake/login.php' ) ;
 	//}
 	
-	
+	include('header.php'); //add header.php to page
 	?>
-	<!DOCTYPE html>
-<html lang="en">
-<head>
-	<title>People</title>
-	<meta charset="utf-8" />
-	<link href='http://fonts.googleapis.com/css?family=Merriweather+Sans:400,400italic,700,700italic' rel='stylesheet' type='text/css' />
-	<link href="styles.css" rel="stylesheet" type="text/css" />
-	<script src="libraries/jquery-1.10.2.min.js" type="text/javascript"></script>
-</head>
-
-<body>
-<header id="site-header" class="site-header">
-	<h1 class="site-title">Time Tracker</h1>
-	<nav id="site-nav" class="site-nav">
-		<ul id="site-menu" class="site-menu">
-			<li class="site-menu-item"><a class="site-menu-link" href="#">Timesheets</a></li>
-			<li class="site-menu-item"><a class="site-menu-link" href="#">Reports</a></li>
-			<li class="site-menu-item"><a class="site-menu-link" href="#">Invoices</a></li>
-			<li class="site-menu-item"><a class="site-menu-link" href="manage.html">Manage</a></li>
-		</ul>
-	</nav>
-	<nav id="section-nav" class="section-nav manage">
-		<h1 class="section-nav-title">Manage: </h1>
-		<ul class="section-menu">
-			<li class="section-menu-item"><a class="section-menu-link" href="projects.php">Projects</a></li>
-			<li class="section-menu-item"><a class="section-menu-link" href="clients.php">Clients</a></li>
-			<li class="section-menu-item"><a class="section-menu-link" href="people.php">Team</a></li>
-		</ul>
-	</nav>
-</header>
 <section id="page-content" class="page-content">
 	<header class="page-header">
 		<h1 class="page-title">People</h1>
 		<nav class="page-controls-nav">
-			<ul class="client-page-controls">
-				<li class="page-controls-item add-client-button"><a class="add-client-link" href="person-add.php">+ Add Person</a></li>
-				<!--<li class="page-controls-item add-client-button"><a class="add-client-link" href="person-add.html">+ Add Client</a></li>-->
-				<li class="page-controls-item"><a class="view-client-archive-link" href="project_archives.php">View Project Archives</a></li>
+			<ul class="page-controls-list project">
+				<li class="page-controls-item link-btn"><a class="add-person-link" href="person-add.php">+ Add Person</a></li>
+				<!-- <li class="page-controls-item"><a class="view-client-archive-link" href="project_archives.php">View Project Archives</a></li> -->
 			</ul>
 		</nav>
 	</header>
