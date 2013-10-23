f2eab8cc20348ee75f7d6134dde2ce5807589582
catsbap <catsbap@gmail.com>
2013-10-22 22:00:53 -0400

    new file for adding tasks

--- ui/tasks.php
+++ ui/tasks.php
@@ -1,6 +1,7 @@
 <?php
 	require_once("../common/common.inc.php");
 	require_once("../classes/Task.class.php");
+	require_once("../common/errorMessages.php");
 		//remove auth
 		//if(!isUserLoggedIn()){
 		//redirect if user is not logged in.
@@ -8,8 +9,17 @@
 		//header( 'Location: http://localhost:8888/time_tracker/usercake/login.php' ) ;
 	//}
 	
-	
+	//OVERALL CONTROL
+	//I need this code to be first so I can redirect the page. We may need to do this for others
+	//in this page, display is integrated with the add feature (no beeg)			
+		if (isset($_POST["action"]) and $_POST["action"] == "task-add") {
+				processTask();
+		} else {
+				displayTaskInsertForm(array(), array(), new Task(array()));
+		} 
 
+
+function displayTaskInsertForm($errorMessages, $missingFields, $task) {
 include('header.php'); //add header.php to page
 ?>
 <section id="page-content" class="page-content">
@@ -18,17 +28,51 @@ include('header.php'); //add header.php to page
 		<nav class="page-controls-nav">
 			<ul class="client-page-controls">
 				<li class="page-controls-item add-client-button"><a class="add-client-link" href="task-add.php">+ Add Task</a></li>
-				<!--<li class="page-controls-item add-client-button"><a class="add-client-link" href="person-add.html">+ Add Client</a></li>-->
 				<li class="page-controls-item"><a class="view-client-archive-link" href="task_archives.php">View Task Archives</a></li>
 			</ul>
 		</nav>
 	</header>
 		<?php 
-		//this is the add task UI (IT IS NOT SEPARATE IN THIS MODULE!!!)
+		//this is the add task UI (IT IS NOT SEPARATE IN THIS MODULE!!!)?>
+		<form action="tasks.php" method="post" style="margin-bottom:50px;" enctype="multipart/form-data">
+      <input type="hidden" name="action" value="task-add"/>
+		<section class="client-detail l-col-80">
+        	<fieldset class="client-details-entry">
+				<legend class="client-details-title">Enter task details:</legend>
+				<header class="client-details-header">
+					<h1 class="client-details-title">Enter task details:</h1>
+					<h4 class="required">= Required</h4>
+				</header>
+				<ul class="details-list client-details-list">
+						<label for="client-name" <?php validateField("task_name", $missingFields)?> class="client-details-label">Task Name:</label>
+						<input id="client-name" name="task-name" class="client-name-input" type="text" tabindex="1" value="<?php echo $task->getValueEncoded("task_name")?>" /><br />
+					</li>
+					<li class="client-details-item phoneNum">
+						<label for="client-phone" <?php validateField("task_hourly_rate", $missingFields)?> class="client-details-label">Hourly Rate:</label>
+						<input id="client-phone" name="task-hourly-rate" class="client-phone-input" type="text" tabindex="2" value="<?php echo $task->getValueEncoded("task_hourly_rate")?>" />
+					</li>
+					<label for="contact-info-sync" class="client-details-label">Billable By Default:</label>
+						<input id="contact-info-sync" name="task-bill-by-default" class="contact-info-sync-input" type="checkbox" tabindex="11" value="1" /><br/>
+					<label for="contact-info-sync" class="client-details-label">Common Task (added to all future projects):</label>
+					<input id="contact-info-sync" name="task-common" class="contact-info-sync-input" type="checkbox" tabindex="11" value="1" />
+
+				<fieldset class="client-details-entry">
+				<ul class="details-list client-details-submit">
+					<li class="client-details-item submit-client">
+						<label for="client-add-btn" class="client-details-label"></label>
+						<!--modified field to be of type submit instead of button-->
+                        <input id="client-add-btn" name="task-add-btn" class="client-add-btn" type="submit" value="+ Add Task" tabindex="11"/> 
+						<label for="client-add-btn" class="client-details-label"></label>
+						<!--modified field to be of type submit instead of button-->
+                        <input id="client-add-btn" name="task-add-to-all-btn" class="client-add-btn" type="submit" value="+ Add Task To All Current Projects" tabindex="11"/> 
+					</li>
+				</ul>
+			</fieldset>
+</section>
+</form>
+
 		
-		
-		
-		
+		<?php
 		//this is the display of all tasks.
 			//1. Get out the task types, this is ugly but it works. Could have called a bunch of functions to get this right but NAAAAAH!!
 			list($tasks) = Task::getTasks(); 
@@ -37,81 +81,146 @@ include('header.php'); //add header.php to page
 				<li style="background-color:lightblue;" class="client-info-contact">Tasks common to all projects</li>
 				<!--billable tasks-->
 				<li style="background-color:lightgray;" class="client-info-contact">Task billable by default</li>
-				<?php foreach($tasks as $task) {
-					if ($task->getValue("task_common")) {
-							if ($task->getValue("task_bill_by_default")) {?>
-								<section class="content">
-								<ul id="client-list" class="client-list">
-								<li class="client-list-item l-col-33">
-								<ul class="client-info-list">
-								<li class="client-info-contact"><a class="client-info-contact-link" href="#" title="View contact details"><button>Edit</button></a>  <?php echo ($task->getValue("task_name")); ?></li>
-								<br/><hr/>
-								</ul>		
-								</li>
-								</ul>
-								</section>
-							<?php }
-					}
-				}?>
+			<?php foreach($tasks as $task) {
+				if ($task->getValue("task_common")) {
+						if ($task->getValue("task_bill_by_default")) {
+							$task_id = Task::getTaskId($task->getValue("task_name"))
+						?>
+							<section class="content">
+							<ul id="client-list" class="client-list">
+							<li class="client-list-item l-col-33">
+							<ul class="client-info-list">
+							<li class="client-info-contact"><a class="client-info-contact-link" href="task-edit.php?task_id=<?php echo $task_id[0]; ?>" title="View contact details"><button>Edit</button></a>  <?php echo ($task->getValue("task_name")); ?></li>
+							<br/><hr/>
+							</ul>		
+							</li>
+							</ul>
+							</section>
+						<?php }
+				}
+			}?>
 				<!--non billable tasks-->
 				<li style="background-color:lightgray;" class="client-info-contact">Non-billable by default</li>
-					<?php foreach($tasks as $task) {
-					if ($task->getValue("task_common")) {
-							if (!$task->getValue("task_bill_by_default")) {?>
-								<section class="content">
-								<ul id="client-list" class="client-list">
-								<li class="client-list-item l-col-33">
-								<ul class="client-info-list">
-								<li class="client-info-contact"><a class="client-info-contact-link" href="#" title="View contact details"><button>Edit</button></a>  <?php echo ($task->getValue("task_name")); ?></li>
-								<br/><hr/>
-								</ul>		
-								</li>
-								</ul>
-								</section>
-							<?php }
-					}
+			<?php foreach($tasks as $task) {
+				if ($task->getValue("task_common")) {
+						if (!$task->getValue("task_bill_by_default")) {?>
+							<section class="content">
+							<ul id="client-list" class="client-list">
+							<li class="client-list-item l-col-33">
+							<ul class="client-info-list">
+							<li class="client-info-contact"><a class="client-info-contact-link" href="#" title="View contact details"><button>Edit</button></a>  <?php echo ($task->getValue("task_name")); ?></li>
+							<br/><hr/>
+							</ul>		
+							</li>
+							</ul>
+							</section>
+					<?php }
 				}
+			}
 				//other tasks here
 				?>
 				<li style="background-color:lightblue;" class="client-info-contact">Other tasks</li>
 				<!--billable tasks-->
 				<li style="background-color:lightgray;" class="client-info-contact">Task billable by default</li>
-				<?php foreach($tasks as $task) {
-					if (!$task->getValue("task_common")) {
-							if ($task->getValue("task_bill_by_default")) {?>
-								<section class="content">
-								<ul id="client-list" class="client-list">
-								<li class="client-list-item l-col-33">
-								<ul class="client-info-list">
-								<li class="client-info-contact"><a class="client-info-contact-link" href="#" title="View contact details"><button>Edit</button></a>  <?php echo ($task->getValue("task_name")); ?></li>
-								<br/><hr/>
-								</ul>		
-								</li>
-								</ul>
-								</section>
-							<?php }
-					}
-				}?>
+			<?php foreach($tasks as $task) {
+				if (!$task->getValue("task_common")) {
+						if ($task->getValue("task_bill_by_default")) {?>
+							<section class="content">
+							<ul id="client-list" class="client-list">
+							<li class="client-list-item l-col-33">
+							<ul class="client-info-list">
+							<li class="client-info-contact"><a class="client-info-contact-link" href="#" title="View contact details"><button>Edit</button></a>  <?php echo ($task->getValue("task_name")); ?></li>
+							<br/><hr/>
+							</ul>		
+							</li>
+							</ul>
+							</section>
+						<?php }
+				}
+			}?>
 				<!--non billable tasks-->
 				<li style="background-color:lightgray;" class="client-info-contact">Non-billable by default</li>
-					<?php foreach($tasks as $task) {
-					if (!$task->getValue("task_common")) {
-							if (!$task->getValue("task_bill_by_default")) {?>
-								<section class="content">
-								<ul id="client-list" class="client-list">
-								<li class="client-list-item l-col-33">
-								<ul class="client-info-list">
-								<li class="client-info-contact"><a class="client-info-contact-link" href="#" title="View contact details"><button>Edit</button></a>  <?php echo ($task->getValue("task_name")); ?></li>
-								<br/><hr/>
-								</ul>		
-								</li>
-								</ul>
-								</section>
-							<?php }
-					}
+			<?php foreach($tasks as $task) {
+				if (!$task->getValue("task_common")) {
+						if (!$task->getValue("task_bill_by_default")) {?>
+							<section class="content">
+							<ul id="client-list" class="client-list">
+							<li class="client-list-item l-col-33">
+							<ul class="client-info-list">
+							<li class="client-info-contact"><a class="client-info-contact-link" href="#" title="View contact details"><button>Edit</button></a>  <?php echo ($task->getValue("task_name")); ?></li>
+							<br/><hr/>
+							</ul>		
+							</li>
+							</ul>
+							</section>
+						<?php }
 				}
+			}
+
+}
+
+function processTask() {
+ 	//these are the required task fields in this form
+	$requiredFields = array("task_name");
+	$missingFields = array();
+	$errorMessages = array();
+	
+	//create the task object ($task)
+	$task = new Task( array(
+		//CHECK REG SUBS!!
+		"task_name" => isset($_POST["task-name"]) ? preg_replace("/[^ \-\_a-zA-Z0-9]/", "", $_POST["task-name"]) : "",
+		"task_hourly_rate" => isset($_POST["task-hourly-rate"]) ? preg_replace("/[^ \$\.\-\_a-zA-Z0-9]/", "", $_POST["task-hourly-rate"]) : "",
+		"task_bill_by_default" => isset($_POST["task-bill-by-default"]) ? preg_replace("/[^ \_0-9]/", "", $_POST["task-bill-by-default"]) : "",
+		"task_common" => isset($_POST["task-common"])? preg_replace("/[^ \_0-9]/", "", $_POST["task-common"]) : "",
+	));
+	
+	
+//error messages and validation script
+	foreach($requiredFields as $requiredField) {
+		if ( !$task->getValue($requiredField)) {
+			$missingFields[] = $requiredField;
+		}	
+	}
+	
+	
+	if ($missingFields) {
+		$i = 0;
+		$errorType = "required";
+		foreach ($missingFields as $missingField) {
+			$errorMessages[] = "<li>" . getErrorMessage(1,$missingField, $errorType) . "</li>";
+			$i++;
+		}
+	} 
+		
+	if ($errorMessages) {
+		displayTaskInsertForm($errorMessages, $missingFields, $task);
+	} else {
+		//steal this code for people. 
+		$task_name=$task->getValue("task_name");
+		$task_id = $task->getTaskId($task_name);
+		if ($task_id) {
+			echo "Task " . $task->getValue("task_name") . " is already in the database. Please try again.";
+		} else {
+			error_log("here is the post");
+			error_log(print_r($_POST,true));
+			error_log("here is the task");
+			error_log(print_r($task,true));
+			try	{
+				$task->insertTask();
+				echo("Task " . $task->getValue('task_name') . " has been successfully added to the database.");
+			} catch (Error $e) {
+				echo "Something went terribly wrong.";
+			}
+			
+		}
+		//headers already sent, call the page back with blank attributes.
+		displayTaskInsertForm(array(), array(), $task);
+	}
+} 
+
 ?>
 </section>
+
 <footer id="site-footer" class="site-footer">
 
 </footer>
