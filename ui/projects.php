415ec8dbaed1ade3b0e988f9825e34ddb95fb8cf
catsbap <catsbap@gmail.com>
2013-10-27 08:53:05 -0400

    added code for archiving the projects

--- ui/projects.php
+++ ui/projects.php
@@ -1,6 +1,34 @@
 <?php
 	require_once("../common/common.inc.php");
 	require_once("../classes/Project.class.php");
+	require_once("../classes/Client.class.php");
+	
+	//this page doubles as the project archive page, so get the value off the get and display those projects here.
+	$archivedView = 0;
+	if (isset($_GET["archives"])) {
+		$archivedView = $_GET["archives"];
+	} elseif (isset($_POST["archives"])) {
+		//print_r($_POST["archives"]);
+		$archivedView = $_POST["archives"];
+	} else {
+		$archivedView = 0;
+	}
+	
+	
+	$project_id = "";
+	if (isset($_POST["change_archive"])) {
+		$project_id = $_POST["change_archive"];
+	}
+	
+	//HOLY FUNKY LOGIC, BATMAN!!
+	if (isset($_POST["change_archive"])) {
+		if ($archivedView) {
+			Project::setArchiveFlag('0', $project_id);
+		} else {
+			Project::setArchiveFlag('1', $project_id);
+		}
+	}
+
 	
 	//if(!isUserLoggedIn()){
 	//	//redirect if user is not logged in.
@@ -18,6 +46,8 @@
 
 <section id="page-content" class="page-content">
 	<section class="content">
+	<form action="projects.php" method="post" style="margin-bottom:50px;">
+
 		<table id="project-list" class="entity-table projects tablesorter">
 			<thead>
 				<tr>
@@ -28,22 +58,31 @@
 				</tr>
 			</thead>
 			<tbody>
-			<?php $clientList = Project::getClientsProjectsByStatus(0);
-				foreach($clientList as $clients) {
-					//get the array of project objects for this client.
-					$projects = Project::getProjectByClientId($clients["client_id"]);
-					foreach ($projects as $project) { 
+			<input type="hidden" value="<?php echo $archivedView ?>" name="archives">
+			<?php $projectList = Project::getClientsProjectsByStatus($archivedView);
+				foreach($projectList as $project) {
 					?>
 						<tr>
-							<!-- <td><input id="select-project" name="select-project" type="checkbox" value="all" title="Select project" /> -->
-							<td><a class="project-info-name-link" href="<?php echo "project-detail.php?project_id=" . $project->getValueEncoded("project_id")?>" title="View project details"><?php echo $project->getValueEncoded("project_name")?></a></td>
-							<td><a class="client-info-contact-link" href="<?php echo "client-detail.php?client_id=" . $clients["client_id"]?>" title="View client details"><?php echo $clients["client_name"] ?></a></td>
+
+						<!-- <td><input id="select-project" name="select-project" type="checkbox" value="all" title="Select project" /> -->
+							<td>
+<?php if ($archivedView) {
+	$buttonTitle = "Unarchive this Project";
+} else {
+	$buttonTitle = "Archive this Project";
+}
+?>
+<a class="project-info-name-link" href="<?php echo "project-detail.php?project_id=" . $project->getValueEncoded("project_id")?>" title="View project details"><?php echo $project->getValueEncoded("project_name")?></a>   							<?php $clientNames = Client::getClientNameById($project->getValueEncoded("client_id")) ?> <td><button type="submit" name="change_archive" value="<?php echo $project->getValueEncoded('project_id'); ?>"><?php echo $buttonTitle ?></button></td>
+
+							<td><a class="client-info-contact-link" href="<?php echo "client-detail.php?client_id=" . $project->getValueEncoded("client_id")?>" title="View client details"><?php echo  $clientNames["client_name"]?></a></td>
 							<td>x Hours/y budget</td>
+
 						</tr>
-					<?php } ?>
 				<?php } ?>
 			</tbody>
 		</table>
+		</form>
+
 	</section>
 		
 </section>
