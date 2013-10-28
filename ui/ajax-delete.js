6544277f8db59977acd376a31950585c56d9e99b
catsbap <catsbap@gmail.com>
2013-10-27 20:50:10 -0400

    added function to delete person

--- ui/ajax-delete.js
+++ ui/ajax-delete.js
@@ -1,4 +1,3 @@
-
 $(document).ready();
 function check_projects(){
     $.ajax({
@@ -11,6 +10,21 @@ function check_projects(){
             }
             else if(response=='0'){
             $('#error').css({'color':'red','display':'block'}).html('You may not delete a client with active projects.')
+            }
+        }
+    });
+};
+function check_person(){
+    $.ajax({
+        type:'POST',
+        url:'delete_person_only.php',
+        data:"person_id="+$('#person_id').val(),
+        success:function(response){
+            if(response=='1'){
+            $('#error').css({'color':'#0c0','display':'block'}).html('You successfully deleted the person.')
+            }
+            else if(response=='0'){
+            $('#error').css({'color':'red','display':'block'}).html('You may not delete a person with active projects.')
             }
         }
     });
