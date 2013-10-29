e95530624edd6c743503cd9842442356fdfc0f4e
catsbap <catsbap@gmail.com>
2013-10-29 13:27:52 -0400

    added code to show projects

--- ui/clients.php
+++ ui/clients.php
@@ -3,6 +3,8 @@
 	//require_once($_SERVER["DOCUMENT_ROOT"] . "/usercake/models/config.php");
 	require_once("../classes/Client.class.php");
 	require_once("../classes/Contact.class.php");
+	require_once("../classes/Project.class.php");
+
 
 //removed auth via userCake re:keith 10/17	
 //if(!isUserLoggedIn()){
@@ -36,16 +38,16 @@
 				//RETRIEVE THE CLIENT ID
 				$client_id = Client::getClientId($client->getValueEncoded("client_name"));
 				//GET OUT THE ARCHIVE FLAG
-				$archive_flag = Client::getArchiveFlag($client_id[0]); 
+				$archive_flag = Client::getArchiveFlag($client->getValueEncoded("client_id")); 
 				//THIS IS NOT THE WAY TO DO THIS. 
 				if (isset($_POST["client-archive-btn"]) && $_POST["client-archive-btn"] == $client_id[0]){
-					//echo "you clicked the archive button on the client page.";
 					$archive_flag = 1;
-					Client::setArchiveFlag($archive_flag, $client_id[0]);
-					$archive_flag = Client::getArchiveFlag($client_id[0]);
+					Client::setArchiveFlag($archive_flag, $client->getValueEncoded("client_id"));
+					$archive_flag = Client::getArchiveFlag($client->getValueEncoded("client_id"));
 				}
 				//GET THE PRIMARY CONTACT OUT. ALL CONTACTS MUST HAVE A PRIMARY CONTACT, CHECK THE FLAG IN THE CLIENT TABLE.
-				$primary_contact = Contact::getPrimaryContact($client_id[0]);
+				$primary_contact = Contact::getPrimaryContact($client->getValueEncoded("client_id"));
+				//this is actually setting the value in the $primary_contact array.
 				if (!isset($primary_contact)) $primary_contact = new Contact(array("contact_name"=>"No contacts found, please investigate why this client doesn't have a contact."));
 				
 				//DISPLAY ONLY ACTIVE CLIENTS, ARCHIVED CLIENTS ARE ON THE ARCHIVE PAGE.
@@ -54,9 +56,22 @@
 			<li class="entity-list-item client l-col-33">
 				<img class="client-logo-thumbnail thumbnail" src="<?php echo $client->getValueEncoded("client_logo_link")?>" title="Client Logo" alt="Client Logo" />
 				<ul class="entity-info-list client">
-					<li class="entity-info-item client"><a class="client-info-name-link" href="<?php echo "client-detail.php?client_id=" . $client_id[0]?>" title="View client details"><?php echo $client->getValueEncoded("client_name")?></a></li>
+					<li class="entity-info-item client"><a class="client-info-name-link" href="<?php echo "client-detail.php?client_id=" . $client->getValueEncoded("client_id")?>" title="View client details"><?php echo $client->getValueEncoded("client_name")?></a></li>
 					<li class="entity-info-item client">Contact: <a class="client-info-contact-link" href="#" title="View contact details"><?php echo $primary_contact->getValue("contact_name") ?></a></li>
-					<li class="entity-info-item client">X Active <a class="client-info-active-projects-link" href="#" title="View active projects">Projects</a></li>
+					
+					<?php 
+					//we'll use an existing function to work this magic. Get all the clients with
+					//active projects and then display them by count for a particular client.
+					$clientProjects = Project::getClientsProjectsByStatus(1);
+					$activeProjectCount = 0;
+					foreach($clientProjects as $clientProject) {
+						if ($client->getValueEncoded("client_id") == $clientProject->getValueEncoded("client_id")) {
+							$activeProjectCount++;
+						}
+					}
+					?>
+					
+					<li class="entity-info-item client"><?php echo $activeProjectCount; ?> Active <a class="client-info-active-projects-link" href="projects.php" title="View active projects">Projects</a></li>
 					<!--
 <form action="clients.php" method="post" style="margin-bottom:50px;">
 						<button id="client-archive-btn" name="client-archive-btn" class="client-archive-btn" type="submit" value="<?php echo $client_id[0] ?>" tabindex="11">Archive Client</button></form>
