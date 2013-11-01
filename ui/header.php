cf3e8d531c0a998ad8785252789a6f7b3d6b4754
catsbap <catsbap@gmail.com>
2013-11-01 14:29:00 -0400

    checking that the $_SESSION is set and doing some lame hide show for auth

--- ui/header.php
+++ ui/header.php
@@ -1,4 +1,8 @@
-<?php ?>
+<?php
+	require_once("../classes/Person.class.php");
+	require_once("../classes/Person_Permissions.class.php");
+
+?>
 
 <!DOCTYPE html>
 <?php
@@ -28,22 +32,38 @@ Edit this file for updating links to pages/screens
 </head>
 
 <body>
+<?php $person = Person::getByEmailAddress($_SESSION["person"]);
+$person_perms = Person_Permissions::getPermissionsAsObject($person->getValueEncoded("person_id"));
+?> 
 <header id="site-header" class="site-header">
 	<h1 class="site-title">Time Tracker</h1>
 	<nav id="site-nav" class="site-nav">
 		<ul id="site-menu" class="site-menu">
 			<li class="site-menu-item"><a class="site-menu-link" href="#">Timesheets</a></li>
+			<?php if ($person_perms->getValueEncoded("person_perm_id") != "Regular User") {?>
 			<li class="site-menu-item"><a class="site-menu-link" href="#">Reports</a></li>
 			<li class="site-menu-item"><a class="site-menu-link" href="#">Invoices</a></li>
 			<li class="site-menu-item"><a class="site-menu-link" href="manage.php">Manage</a></li>
-			
+		<?php } ?>	
 		</ul>
 	</nav>
 	<nav id="util-nav" class="util-nav">
 		<ul id="util-menu" class="util-menu">
-			<li class="section-menu-item"><a class="section-menu-link" href="logout.php">Log Out</a></li>
+			<li class="section-menu-item"><a class="section-menu-link" href="logout.php">Log Out <?php echo $_SESSION["person"];?></a></li>
 		</ul>
 	</nav>
+	<nav id="section-nav" class="section-nav manage">
+		<h1 class="section-nav-title">Timesheets: </h1>
+		<ul class="section-menu">
+			<li class="section-menu-item"><a class="section-menu-link" href="timesheet.php">Time</a></li>
+			<li class="section-menu-item"><a class="section-menu-link" href="#">Expenses</a></li>
+			<li class="section-menu-item"><a class="section-menu-link" href="#">Pending Approval</a></li>
+			<li class="section-menu-item"><a class="section-menu-link" href="#">Unsubmitted</a></li>
+			<li class="section-menu-item"><a class="section-menu-link" href="#">Archive</a></li>
+
+		</ul>
+	</nav>
+	<?php if ($person_perms->getValueEncoded("person_perm_id") != "Regular User") {?>
 	<nav id="section-nav" class="section-nav manage">
 		<h1 class="section-nav-title">Manage: </h1>
 		<ul class="section-menu">
@@ -53,4 +73,5 @@ Edit this file for updating links to pages/screens
 			<li class="section-menu-item"><a class="section-menu-link" href="tasks.php">Tasks</a></li>
 		</ul>
 	</nav>
+	<?php } ?>
 </header>
