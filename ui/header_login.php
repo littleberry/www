53d71e053e2f17268b16bbdfe8929175b2bb373c
catsbap <catsbap@gmail.com>
2013-11-06 11:57:14 -0500

    header_login, used when the user is on a screen that should not be signed in.

---
+++ ui/header_login.php
@@ -0,0 +1,20 @@
+<?php
+	require_once("../classes/Person.class.php");
+	require_once("../classes/Person_Permissions.class.php");
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<title>Manage</title>
+	<meta charset="utf-8" />
+	<link href='http://fonts.googleapis.com/css?family=Merriweather+Sans:400,400italic,700,700italic' rel='stylesheet' type='text/css' />
+	<link href="styles.css" rel="stylesheet" type="text/css" />
+	<script src="libraries/jquery-1.10.2.min.js" type="text/javascript"></script>
+</head>
+
+<body>
+<header id="site-header" class="site-header">
+	<h1 class="site-title">Time Tracker</h1><a href="login.php"><h3 class="site-title">Log In</h3></a>
+</header>
