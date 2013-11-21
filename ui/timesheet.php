c3dda43567f38ef29ea91194f19833589e50e03c
catsbap <catsbap@gmail.com>
2013-11-21 11:53:28 -0500

    just added a comment.

--- ui/timesheet.php
+++ ui/timesheet.php
@@ -352,6 +352,7 @@ function saveTimesheetOld() {
 		//*INSERT THE TIMESHEET (ADD IT TO THE DB)
 		$timesheet_object->updateTimesheet($timesheet_object->getValueEncoded("timesheet_start_date"),$timesheet_object->getValueEncoded("timesheet_end_date"));		//*INSERT THE TIMESHEET ITEM (ADD IT TO THE DB)
 		error_log("checking value of lastInsertId:" . $lastInsertId[0]);
+		//this isn't right, this needs to also send the project ID and the task_id. Otherwise, timesheet_id is not specific enough.
 		$timesheet_object->insertTimesheetItem($timesheet_object->getValueEncoded("person_id"), $lastInsertId[0]);
 	}	
 			displayTimesheet($timesheet_aggregate, $timesheet_item_aggregate);
