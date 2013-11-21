846b9045893a69e62d9d16790045c23b53b680e8
catsbap <catsbap@gmail.com>
2013-11-21 11:52:56 -0500

    moved a function for approval of time sheets to timesheet_items class.

--- classes/Timesheet.class.php
+++ classes/Timesheet.class.php
@@ -15,21 +15,20 @@ class Timesheet extends DataObject {
 	);
 	
 	
-	//display all information about a timesheet for a person, including the details, returned as an array of timesheet objects.
-	//leave this here, but pretty sure this isn't going to be how we use this.
-	public function getTimesheetDetail($person_id) {
+	//display all information about a timesheet for a person, including the details and whether it is submitted, returned as an array of timesheet_item objects. 
+	public function getSubmittedTimesheetDetail($timesheet_id, $is_submitted) {
 		$conn=parent::connect();
 		//$sql="SELECT * FROM " . TBL_TIMESHEET . " WHERE timesheet_id = :timesheet_id";
 		//We're going to need the aggregate at some point so I'll stick it here.
-		//SELECT timesheet.*, sum(timesheet_detail.timesheet_number_of_hours)  as totalhours FROM timesheet, timesheet_detail where timesheet_detail.timesheet_id = timesheet.timesheet_id group by timesheet.timesheet_id;
-		$sql = "SELECT ts.*, td.* FROM " . TBL_TIMESHEET . " as ts, " . TBL_TIMESHEET_ITEM . " as td WHERE ts.timesheet_id = td.timesheet_id AND ts.person_id = :person_id";
+		$sql = "SELECT ts.*, td.* FROM " . TBL_TIMESHEET . " as ts, " . TBL_TIMESHEET_ITEM . " as td WHERE ts.timesheet_id = td.timesheet_item_id";
 		try {
 			$st = $conn->prepare($sql);
-			$st->bindValue(":person_id", $person_id, PDO::PARAM_INT);
+			$st->bindValue(":timesheet_id", $timesheet_id, PDO::PARAM_INT);
 			$st->execute();
 			$timesheet_details=array();
 			foreach ($st->fetchAll() as $row) {
 				$timesheet_item[] = $row;
+				error_log(print_r($row,true));
 			}
 			parent::disconnect($conn);
 			return array($timesheet_item);
@@ -39,17 +38,21 @@ class Timesheet extends DataObject {
 		}
 	}
 	
-	//get all of the timesheets for a specific person and date.
-	public function getTimesheetByPersonForDate($person_id, $timesheet_date) {
+	
+	//get all of the submitted timesheets for a specific person (this just does the assignee right now, but it should include all people that are administrators as well).
+	//maybe re-write this as a join. This is going to be slow and is confusing.
+	public function getSubmittedTimesheetsByManager($manager_email) {
 		$conn=parent::connect();
-		$sql="SELECT distinct(timesheet_id) FROM " . TBL_TIMESHEET . " WHERE person_id = :person_id and timesheet_id in (select timesheet_id from " . TBL_TIMESHEET_ITEM . " WHERE timesheet_date = :timesheet_date)";
+		error_log($manager_email);
+		$sql="SELECT timesheet_id FROM " . TBL_TIMESHEET . " WHERE timesheet_id in (select timesheet_item_id from " . TBL_TIMESHEET_ITEM . " WHERE project_id in (select project_id from " . TBL_PROJECT_PERSON . " WHERE project_assigned_by = :manager_email)) and timesheet_submitted = 1";
 		try {
 			$st = $conn->prepare($sql);
-			$st->bindValue(":person_id", $person_id, PDO::PARAM_INT);
-			$st->bindValue(":timesheet_date", date('y-m-d', strtotime($timesheet_date)), PDO::PARAM_STR);
+			$st->bindValue(":manager_email", $manager_email, PDO::PARAM_STR);
+			//$st->bindValue(":timesheet_date", date('y-m-d', strtotime($timesheet_date)), PDO::PARAM_STR);
 			$st->execute();
 			$timesheet=array();
 			foreach ($st->fetchAll() as $row) {
+				error_log(print_r($row,true));
 				$timesheet[] = new Timesheet($row);
 			}
 			parent::disconnect($conn);
@@ -73,7 +76,7 @@ class Timesheet extends DataObject {
 				$timesheet[] = new Timesheet($row);
 			}
 			parent::disconnect($conn);
-			return array($timesheet);
+			return $timesheet;
 		}catch(PDOException $e) {
 			parent::disconnect($conn);
 			die("query failed here: " . $e->getMessage() . "query is " . $sql);
@@ -168,7 +171,7 @@ class Timesheet extends DataObject {
 		}
 	}
 	
-	//update the timesheet data.
+//update the timesheet data.
 	public function updateTimesheet($timesheet_start_date, $timesheet_end_date) {
 		$conn=parent::connect();
 		$sql = "UPDATE " . TBL_TIMESHEET . " SET
@@ -193,249 +196,25 @@ class Timesheet extends DataObject {
 		}
 
 	}
+	
+	//submit the timesheet.
+	public function submitTimesheet($timesheet_id) {
+		$conn=parent::connect();
+		$sql = "UPDATE " . TBL_TIMESHEET . " SET
+			timesheet_submitted = 1
+			WHERE timesheet_id = :timesheet_id";
+		try {
+			$st = $conn->prepare($sql);
+			$st->bindValue(":timesheet_id", $timesheet_id, PDO::PARAM_STR);
+			$st->execute();
+			parent::disconnect($conn);
+		} catch (PDOException $e) {
+			parent::disconnect($conn);
+			die("Query failed on update of timesheet, sql is $sql " . $e->getMessage());
+		}
+
+	}
+
 }
 
-
-/*
-	//return all data for a specific client based on the client_id.
-	public function getClient($clientId) {
-		$conn=parent::connect();
-		//OLD SQL, use a left join here, rather than just deliver an error for missing data.
-		//$sql = "SELECT " . TBL_CLIENT . ".*," . TBL_CLIENT_ADDRESS . ".* FROM " . TBL_CLIENT . "," . TBL_CLIENT_ADDRESS . " WHERE " . TBL_CLIENT . ".client_id = :client_id and ". TBL_CLIENT . ".client_id=" . TBL_CLIENT_ADDRESS . ".client_id";
-		$sql = "SELECT " . TBL_CLIENT . ".*," . TBL_CLIENT_ADDRESS . ".* FROM " . TBL_CLIENT . " as client LEFT JOIN " . TBL_CLIENT_ADDRESS . " as client_address on client.client_id = client_address.client_id WHERE client.client_id = :client_id";
-		try {
-			$st = $conn->prepare($sql);
-			$st->bindValue(":client_id", $clientId, PDO::PARAM_INT);
-			$st->execute();
-			$row=$st->fetch();
-			parent::disconnect($conn);
-			if ($row) return new Client($row);
-		} catch(PDOException $e) {
-			parent::disconnect($conn);
-			die("Query failed on you: " . $e->getMessage());
-		}
-	}
-	
-	
-	//return the clients name based on the client_id.
-	//I'll keep this here as a utility function in case we need it.
-	public function getClientNameById($client_id) {
-		$conn=parent::connect();
-		$sql = "SELECT client_name FROM " . TBL_CLIENT . " WHERE client_id = '" . $client_id . "'";			
-		try {
-			$st = $conn->prepare($sql);
-			$st->bindValue(":client_id", $client_id, PDO::PARAM_INT);
-			$st->execute();
-			$row=$st->fetch();
-			parent::disconnect($conn);
-			if ($row) return $row;
-		} catch (PDOException $e) {
-			parent::disconnect($conn);
-			die("Query failed getting the client name, sql is $sql " . $e->getMessage());
-		}
-	}
-	
-	//return the client_id for a specific name.
-	public function getClientId($client_name) {
-		$conn=parent::connect();
-		$sql = "SELECT client_id FROM " . TBL_CLIENT . " WHERE client_name = '" . $client_name . "'";			
-		try {
-			$st = $conn->prepare($sql);
-			$st->bindValue(":client_name", $client_name, PDO::PARAM_STR);
-			$st->execute();
-			$row=$st->fetch();
-			parent::disconnect($conn);
-			if ($row) return $row;
-		} catch (PDOException $e) {
-			parent::disconnect($conn);
-			die("Query failed getting the client id, sql is $sql " . $e->getMessage());
-		}
-	}
-	
-	//get the available currencies out of the currency table
-	//9/4: Client only needs US here, so this only returns USD at this point, but this is built to handle others.
-	public function getCurrency() {
-		$conn=parent::connect();
-		$sql = "SELECT * FROM " . TBL_CURRENCY;
-		
-		try {
-			$st = $conn->prepare($sql);
-			$st->execute();
-			parent::disconnect($conn);
-			$_currency = array();
-			foreach ($st->fetchAll() as $row) {
-				$currency[] = $row;
-			}
-			return $currency;
-		} catch(PDOException $e) {
-			parent::disconnect($conn);
-			die("Query failed on you: " . $e->getMessage());
-		}
-	}
-	
-	//get the currency for a specific currency index
-	public function getCurrencyByIndex($client_currency_index) {
-		$conn=parent::connect($client_currency_index);
-		$sql = "SELECT client_preferred_currency FROM " . TBL_CURRENCY . " WHERE client_currency_index = :client_currency_index";
-		
-		try {
-			$st = $conn->prepare($sql);
-			$st->bindValue(":client_currency_index", $client_currency_index, PDO::PARAM_INT);
-			$st->execute();
-			$row=$st->fetch();
-			parent::disconnect($conn);
-			//just return the value for the currency.
-			if ($row) return $row[0];
-		} catch(PDOException $e) {
-			parent::disconnect($conn);
-			die("Query failed on you: " . $e->getMessage());
-		}
-	}
-				
-		//get the client ID out of the client table based on the email address we just inserted. It must use the same key (auto increment) created when the record
-		//was inserted into the client table.
-		$conn=parent::connect();
-		$sql = "SELECT client_id FROM " . TBL_CLIENT . " WHERE client_email = '" . $client_email . "'";			
-		try {
-			$st = $conn->prepare($sql);
-			$st->bindValue(":client_email", $client_email, PDO::PARAM_STR);
-			$st->execute();
-			$client_id=$st->fetch();
-			parent::disconnect($conn);
-		} catch (PDOException $e) {
-			parent::disconnect($conn);
-			die("Query failed getting the client id, sql is $sql " . $e->getMessage());
-		}
-		//insert the address data into the address table for the appropriate client_id.
-		$conn=parent::connect();
-		$sql = "INSERT INTO " . TBL_CLIENT_ADDRESS . " (
-			client_id,
-			client_address,
-			client_state,
-			client_zip,
-			client_city
-		) VALUES (
-			:client_id,
-			:client_address,
-			:client_state,
-			:client_zip,
-			:client_city
-		)";
-			
-		try {
-			$st = $conn->prepare($sql);
-			$st->bindValue(":client_id", $client_id["client_id"], PDO::PARAM_STR);
-			$st->bindValue(":client_address", $this->data["client_address"], PDO::PARAM_INT);
-			$st->bindValue(":client_state", $this->data["client_state"], PDO::PARAM_STR);
-			$st->bindValue(":client_zip", $this->data["client_zip"], PDO::PARAM_INT);
-			$st->bindValue(":client_city", $this->data["client_city"], PDO::PARAM_STR);
-			$st->execute();
-			parent::disconnect($conn);
-		} catch (PDOException $e) {
-			parent::disconnect($conn);
-			die("Query failed on insert of client address, sql is $sql " . $e->getMessage());
-		}
-	}
-	
-	//update the client record based on the client_id
-	//if we want to break out the address, write the config to do the update later
-	//so that we can update those fields as well.
-	//9/4/13
-	public function updateClient($client_id) {
-		$conn=parent::connect();
-		$sql = "UPDATE " . TBL_CLIENT . " SET
-			client_name = :client_name,
-			client_email = :client_email,
-			client_phone = :client_phone,
-			client_fax = :client_fax,
-			client_currency_index = :client_currency_index,
-			client_logo_link = :client_logo_link,
-			client_archived = :client_archived
-			WHERE client_id = :client_id";
-		try {
-			$st = $conn->prepare($sql);
-			$st->bindValue(":client_name", $this->data["client_name"], PDO::PARAM_STR);
-			$st->bindValue(":client_email", $this->data["client_email"], PDO::PARAM_STR);
-			$st->bindValue(":client_phone", $this->data["client_phone"], PDO::PARAM_INT);
-			$st->bindValue(":client_fax", $this->data["client_fax"], PDO::PARAM_INT);
-			$st->bindValue(":client_archived", $this->data["client_archived"], PDO::PARAM_INT);
-			$st->bindValue(":client_currency_index", $this->data["client_currency_index"], PDO::PARAM_INT);
-			$st->bindValue(":client_logo_link", basename($this->data["client_logo_link"]), PDO::PARAM_STR);
-			$st->bindValue(":client_id", $client_id, PDO::PARAM_INT);
-			$st->execute();	
-			parent::disconnect($conn);
-		} catch (PDOException $e) {
-			parent::disconnect($conn);
-			die("Query failed on update: " . $e->getMessage());
-		}
-		
-		
-	
-	//update the archive flag in the client table.
-	public function setArchiveFlag($flag, $client_id) {
-		$conn=parent::connect();
-		$sql = "UPDATE " . TBL_CLIENT . " SET
-			client_archived = :client_archived
-			WHERE client_id = :client_id";
-	try {
-			$st = $conn->prepare($sql);
-			$st->bindValue(":client_id", $client_id, PDO::PARAM_INT);
-			$st->bindValue(":client_archived", $flag, PDO::PARAM_INT);
-			$st->execute();	
-			parent::disconnect($conn);
-		} catch (PDOException $e) {
-			parent::disconnect($conn);
-			die("Query failed on update: " . $e->getMessage());
-		}
-
-	}
-	
-	//get the archive flag out of the client table.
-	public function getArchiveFlag($client_id) {
-		$conn=parent::connect();
-		$sql = "SELECT client_archived FROM " . TBL_CLIENT . " WHERE client_id = :client_id";
-		
-		try {
-			$st = $conn->prepare($sql);
-			$st->bindValue(":client_id", $client_id, PDO::PARAM_INT);
-			$st->execute();
-			$row=$st->fetch();
-			parent::disconnect($conn);
-			if ($row) return $row[0];
-		} catch(PDOException $e) {
-			parent::disconnect($conn);
-			die("Query failed on you: " . $e->getMessage());
-		}
-	}
-	
-	
-//if you delete a client you delete all of the contacts associated with them as well.
-	public function deleteClient($client_id) {
-		//first delete the contacts.
-		$conn=parent::connect();
-		$sql = "DELETE FROM " . TBL_CONTACT . " WHERE client_id = :client_id";
-		try {
-			$st = $conn->prepare($sql);
-			$st->bindValue(":client_id", $client_id, PDO::PARAM_INT);
-			$st->execute();	
-			parent::disconnect($conn);
-		} catch (PDOException $e) {
-			parent::disconnect($conn);
-			die("Query failed on delete of contact rows.: " . $e->getMessage());
-		}
-		$conn=parent::connect();
-		$sql = "DELETE FROM " . TBL_CLIENT . " WHERE client_id = :client_id";
-		
-		try {
-			$st = $conn->prepare($sql);
-			$st->bindValue(":client_id", $client_id, PDO::PARAM_INT);
-			$st->execute();
-			parent::disconnect($conn);
-			return 1;
-		} catch(PDOException $e) {
-			parent::disconnect($conn);
-			return 0;
-			die("Query failed on delete of client: " . $e->getMessage());
-		}
-	}*/
 ?>
