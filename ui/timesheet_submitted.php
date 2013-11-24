d8c36c5f0bf9e391b5e5d312833f26678c81bfe0
catsbap <catsbap@gmail.com>
2013-11-23 23:16:55 -0500

    commit often

--- ui/timesheet_submitted.php
+++ ui/timesheet_submitted.php
@@ -22,10 +22,10 @@ if (isset($_SESSION["logged_in"]) && $_SESSION["logged_in"] != "") {
 	exit();
 }
 	
-	
-if (isset($_POST["action"]) and $_POST["action"] == "Approve All Timesheets") {
+if (isset($_POST["approve_timesheets"]) and $_POST["approve_timesheets"] == "Approve All Timesheets") {
+//if (isset($_POST["action"]) and $_POST["action"] == "Approve All Timesheets") {
 	approveAllTimesheets();
-} elseif (isset($_POST["action"]) and $_POST["action"] == "Approve Timesheet") {
+} elseif (isset($_POST["approve_timesheets"]) and $_POST["approve_timesheets"] == "Approve Timesheet") {
 	approveTimesheet();
 } else {
 	include('header.php'); //add header.php to page moved to only be called when page is rendered so it's not sent back when page saved via JS/Ajax
@@ -110,7 +110,7 @@ function displayTimesheetApprovalForm($timesheet, $timesheet_item) {
 			$title = "Approve All Timesheets";
 		}	
 		?>
-			<tr><td><input type="submit" value="<?php echo $title?>"></td></tr>
+			<tr><td><input type="submit" name="approve_timesheets" value="<?php echo $title?>"></td></tr>
 		</table>
 		</form>
 		</html>
