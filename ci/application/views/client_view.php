76d2d679530270ec76f8f7e5a2aecc5d52e134d9
catsbap <catsbap@gmail.com>
2013-12-03 20:56:21 -0500

    client_view to add new features

---
+++ ci/application/views/client_view.php
@@ -0,0 +1,18 @@
+	<tr><td colspan=4>
+	<div id="menucss"><?php echo $menu ?></div>
+	</td></tr>
+	<tr><td>
+	<?php foreach ($client_url as $url) {
+		echo $url . "<br>";
+	}
+	echo $controller;
+	//print_r($client_url);
+	?></td></tr>
+	</table>
+	</div>
+	<footer id="site-footer" class="site-footer">
+	
+	</footer>
+
+</body>
+</html>
