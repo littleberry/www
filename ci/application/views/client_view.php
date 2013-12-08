08e1ae8b10d4be75f6578b58ceaa8acc4a9204f7
catsbap <catsbap@gmail.com>
2013-12-08 15:11:56 -0500

    update array output in client report

--- ci/application/views/client_view.php
+++ ci/application/views/client_view.php
@@ -3,28 +3,31 @@
 	</td></tr>
 	<tr><td><h5>Name</h5></td><td><h5>Hours</h5></td><td><h5>Billable Hours</h5></td><td><h5>Billable Amount</h5></td></tr>
 	<?php 
-	foreach ($client_url as $url) {
-			error_log(print_r($url,true));	
-			foreach ($url as $myurl) {
-				print_r($url);
-				
-				if (isset($url['client_url'])) {
-					echo "<tr><td>$myurl</td>";	
-				};
-					
-				if (isset($url['client_time'])) {
-					echo "<td>$myurl</td>";
-				};
-					
-				if (isset($url["client_billable_hours"])) {
-					echo "<td>$myurl</td>";
-				}
-				
-				if (isset($url["client_rate"])) {
-					echo "<td>$myurl</td></tr><tr>";
-				}
-			}
+	print_r($client_url);
+	//var_dump($client_url);
+	foreach ($client_url as $key=>$value) {
+		if(isset($value['client_total_rate'])) {
+			$client_url[$key] = array_merge($client_url[$key], (array) $value['client_total_rate']);
+			unset($client_url[$key]['client_total_rate']);
 		}
+	}
+	echo "<BR><BR>";
+	print_r($client_url);
+	
+	/*foreach($a as $key => $values){
+  if(isset($values['fields']))
+    {
+       $a[$key] = array_merge($a[$key], (array) $values['fields']);
+       unset($a[$key]['fields']);
+    }
+}
+	/*foreach ($client_url as $key=>$value) {	
+		foreach ($client_url[$key] as $clientkey=>$clientvalue) {
+			if(is_int($clientkey)) {
+				$arr['@children'] = call_user_func_array('array_merge', $arr['@children']);
+		}
+	}
+	*/
 	?>
 	</table>
 	</div>
