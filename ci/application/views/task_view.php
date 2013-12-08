b5dddc3326ed2026f5ada15e7da84f39f852511a
catsbap <catsbap@gmail.com>
2013-12-08 15:12:14 -0500

    update array output in client report

--- ci/application/views/task_view.php
+++ ci/application/views/task_view.php
@@ -4,6 +4,7 @@
 	<tr><td><h5>Name</h5></td><td><h5>Hours</h5></td><td><h5>Billable Hours</h5></td><td><h5>Billable Amount</h5></td></tr>
 	<table>
 	<?php 
+	//
 	$i = 0;
 	foreach ($tasks as $task) {
 		echo "<td>$task</td>";
